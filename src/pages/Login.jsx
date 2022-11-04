9f1646bada00cd382045c6240463a4de95a53d0d
Vitória <vitcs11@gmail.com>
2022-11-04 14:59:10 -0300

    Requisito 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,65 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from '../components/Loading';
+import { createUser } from '../services/userAPI';
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      disabledButton: true,
+      loading: false,
+      createUserState: false,
+    };
+    this.insertUser = this.insertUser.bind(this);
+    this.ableBtn = this.ableBtn.bind(this);
+  }
+
+  ableBtn({ target }) {
+    const enabled = (target.value.length > 2);
+    this.setState({
+      name: target.value,
+      disabledButton: !enabled,
+    });
+  }
+
+  async insertUser() {
+    this.setState({
+      loading: true,
+    });
+    const { name } = this.state;
+    await createUser({ name });
+    this.setState({
+      createUserState: true,
+    });
+  }
+
   render() {
+    const { disabledButton, loading, createUserState } = this.state;
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        Login
+        {createUserState && <Redirect to="/search" />}
+        {loading && <Loading />}
+        <form onSubmit={ this.insertUser }>
+          <label htmlFor="name">
+            <input
+              type="text"
+              data-testid="login-name-input"
+              id="name"
+              onChange={ this.ableBtn }
+            />
+          </label>
+          <button
+            type="submit"
+            data-testid="login-submit-button"
+            disabled={ disabledButton }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
     );
   }
 }
