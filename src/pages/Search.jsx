5f7003744b48c7d16477fc7ee1c6db89d9b1b739
Vitória <vitcs11@gmail.com>
2022-11-04 17:08:35 -0300

    Requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,45 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      btnDisabled: true,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const enabled = (target.value.length > 1);
+    this.setState({
+      btnDisabled: !enabled,
+    });
+  }
+
   render() {
+    const { btnDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         Search
+        <form>
+          <label htmlFor="search">
+            <input
+              type="search"
+              data-testid="search-artist-input"
+              id="search"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            disabled={ btnDisabled }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
